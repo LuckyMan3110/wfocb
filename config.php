7e16383a9c4c896f7a42bd21274e908b6c469a2f
LuckyMan3110 <seinom8@gmail.com>
2023-10-16 23:08:42 +0200

    wfocb commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+// $sql_db_name = "wfo_local";
+$sql_db_name = "wfo_live";
+
+// Site URL
+$site_url = "http://localhost/wfocb"; // e.g (http://example.com)
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = "8ac2cb82-bd51-4cf5-afe4-9de52d83c0f0"; // Your purchase code, don't give it to anyone.
+?>
